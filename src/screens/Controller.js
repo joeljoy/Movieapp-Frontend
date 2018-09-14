1ba1b3de9c40565ed06cb9b1ba54e867dbc88ea2
upgrad-edu <versioncontrol@upgrad.com>
2018-09-14 17:33:52 +0530

    latestip.

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://http://13.229.113.25:8080/api/v1/";
+    this.baseUrl = "http://http://54.255.154.11:8080/api/v1/";
   }
   render(){
     return(
